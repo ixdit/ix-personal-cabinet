a75fd035fd16a7c5064f993e85e2ff8b44bf5b45
ixdit <ixdit1979@gmail.com>
2022-09-09 17:20:56 +0300

    добавлены 3 статических эндпоинта

--- classes/class-rest.php
+++ classes/class-rest.php
@@ -6,6 +6,64 @@ class Rest {
 
 	public function init_hooks() {
 
+		add_action( 'rest_api_init', [ $this, 'endpoints' ] );
+
+	}
+
+	public function endpoints() {
+
+		register_rest_route( 'ix/v1/', 'register/', [
+			'methods'  => 'POST',
+			'callback' => [$this, 'register'],
+			'permission_callback' => '__return_true',
+			'args'     => [
+				'username' => [
+					'default' => '',
+				],
+				'email'    => [
+					'default' => '',
+				],
+				'password' => [
+					'password' => '',
+				],
+			],
+		] );
+
+		register_rest_route( 'ix/v1/', 'auth/', [
+			'methods'  => 'POST',
+			'callback' => 'auth',
+			'permission_callback' => function () {
+				return current_user_can( 'edit_others_posts' );
+			},
+			'args'     => [
+				'login' => [
+					'default' => '',
+				],
+				'password' => [
+					'password' => '',
+				],
+			],
+		] );
+
+		register_rest_route( 'ix/v1/', 'reminder/', [
+			'methods'  => 'POST',
+			'callback' => 'reminder',
+			'permission_callback' => function () {
+				return current_user_can( 'edit_others_posts' );
+			},
+			'args'     => [
+				'email'    => [
+					'default' => '',
+				],
+			],
+		] );
+
+	}
+
+	public function register( \WP_REST_Request $request ) {
+
+		print_r($request);
+
 	}
 
 }
