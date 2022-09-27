4d3a1e3b738892af27bb56ce71a4c172a8b5fe47
ixdit <ixdit1979@gmail.com>
2022-09-27 20:40:31 +0300

    переименована функция главного шорт кода, удалены лишние шорт коды

--- classes/class-shortcodes.php
+++ classes/class-shortcodes.php
@@ -8,16 +8,11 @@ class Shortcodes {
 
 	public function init_hooks() {
 
-		add_shortcode( 'ixpc', [ $this, 'ixpc_function' ] );
-
-		add_shortcode( 'ixpc_login', [ $this, 'login' ] );
-		add_shortcode( 'ixpc_auth', [ $this, 'auth' ] );
-		add_shortcode( 'ixpc_reminder', [ $this, 'reminder' ] );
-		add_shortcode( 'ixpc_setpass', [ $this, 'setpass' ] );
+		add_shortcode( 'ixpc', [ $this, 'personal_cabinet' ] );
 
 	}
 
-	public function ixpc_function( $args = [] ) {
+	public function personal_cabinet( $args = [] ) {
 
 		ob_start();
 
@@ -69,66 +64,6 @@ class Shortcodes {
 
 		return ob_get_clean();
 
-	}
-
-
-	public function login( $args = [] ) {
-
-		ob_start();
-
-
-		load_template(
-			ixpc()->templater->get_template( '/login/register.php' ),
-			true,
-			$args
-		);
-
-		return ob_get_clean();
-	}
-
-	public function auth( $args = [] ) {
-
-		ob_start();
-
-		?>
-        <div class="ixpc_forms_wrapper">
-
-        </div>
-		<?php
-
-		load_template(
-			ixpc()->templater->get_template( '/login/auth.php' ),
-			true,
-			$args
-		);
-
-		return ob_get_clean();
-	}
-
-	public function reminder( $args = [] ) {
-
-		ob_start();
-
-		load_template(
-			ixpc()->templater->get_template( '/login/reminder.php' ),
-			true,
-			$args
-		);
-
-		return ob_get_clean();
-	}
-
-	public function setpass( $args = [] ) {
-
-		ob_start();
-
-		load_template(
-			ixpc()->templater->get_template( '/login/setpass.php' ),
-			true,
-			$args
-		);
-
-		return ob_get_clean();
 	}
 
 }
